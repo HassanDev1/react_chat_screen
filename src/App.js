b4c5c435b9c2cf02cf2caf0c37774579ea36ac06
Hassan78 <hassan.ali@g.austincc.edu>
2020-04-01 18:57:42 -0500

    added possible solution"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChatScreen from './chatscreen';
+
+
+// 
+
+// const messages = [
+//   { username: 'Amy', text: 'Hi, Jon!' },
+//   { username: 'Amy', text: 'How are you?' },
+//   { username: 'John', text: 'Hi, Amy! Good, you?' },
+// ];
 
 function App() {
+  const users = [{ username: 'Amy' }, { username: 'John' }];
+  const [messages, setmessages] = useState([]);
+  
+  const handleAdd = (message) => {
+    setmessages([...messages, message]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+      <div className="container">
+        {users.map(user=>
+        <ChatScreen key={user.username} user={user} messages={messages} handleAdd={handleAdd}/>
+        )}
+      </div>
+    </>
   );
+
 }
 
 export default App;
---
+++ src/chatscreen.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+const ChatScreen = (props) => {
+    const [message, setmessage] = useState({ username: props.user.username, text: '' });
+
+    const isDisabled = () => {
+        return message.text === ''
+
+    };
+
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setmessage({
+            ...message,
+            [name]: value,
+            username: props.user.username
+        })
+
+
+
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.handleAdd(message);
+        console.log(message.text);
+
+        setmessage({ text: '' })
+
+    }
+    return (
+
+        <div>
+            <div className="chat-window">
+                <h2>Super Awesome Chat</h2>
+                <div className="name sender">{props.user.username}</div>
+                <ul className="message-list">
+                    {props.messages.map((message, index) => (
+                        <li
+                            key={index}
+                            className={
+                                message.username === props.user.username ? 'message sender' : 'message recipient'
+                            }
+                        >
+                            <p>{`${message.username} : ${message.text}`}</p>
+                        </li>
+                    ))}
+                </ul>
+
+                <div>
+                    <form className="input-group" noValidate onSubmit={handleSubmit}>
+                        <input type="text" name="text"
+                            value={message.text} className="form-control" placeholder="Enter your message..." onChange={handleChange} />
+                        <div className="input-group-append">
+                            <button className="btn submit-button" disabled={isDisabled()} >
+                                SEND
+            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ChatScreen;
